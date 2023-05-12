92ce844cec5a468cb8d2ef17af2cd4f80a9e4da8
shunesoul <shunesoul@gmail.com>
2023-05-12 18:13:06 -0400

    created javascript for night theme. Will impliment a toggle feature later. Also created responsive login page. Still need to apply css to login page

---
+++ code.js
@@ -0,0 +1,11 @@
+function changeBackground() {
+    document.querySelector('.middleSections').style.backgroundColor = 'black'
+    document.querySelector('.middleSections').style.color = 'white';
+    document.querySelector('.body').style.backgroundColor = 'black';
+}
+
+
+
+
+
+
