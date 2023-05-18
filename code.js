582aa974971f07f36f8acebce1316976d78858d4
shunesoul <shunesoul@gmail.com>
2023-05-18 19:56:47 -0400

    created login in page for website and added basic javascript for the confirmation of login

--- code.js
+++ code.js
@@ -4,8 +4,16 @@ function changeBackground() {
     document.querySelector('.body').style.backgroundColor = 'black';
 }
 
+function createAccount() {
+    alert("You have made an account");
+}
 
+function login(){
+    alert("You are logged in");
+}
 
-
+function passwordReminder(){
+    alert("Password reminder sent to email on file");
+}
 
 
