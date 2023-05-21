1820b25630a5f9f5172ad41324414202ee125d05
shunesoul <shunesoul@gmail.com>
2023-05-20 20:06:58 -0400

    updated javascript on the page

--- code.js
+++ code.js
@@ -2,8 +2,13 @@ function changeBackground() {
     document.querySelector('.middleSections').style.backgroundColor = 'black'
     document.querySelector('.middleSections').style.color = 'white';
     document.querySelector('.body').style.backgroundColor = 'black';
+    document.querySelectorAll('.contentSection').forEach((item) => {
+        item.style.border = 'solid 2px grey';
+    });
+    document.querySelectorAll('.lineBreak').forEach((item)=> {
+        item.style.border = 'solid 2px grey';
+    });
 }
-
 function createAccount() {
     alert("You have made an account");
 }
